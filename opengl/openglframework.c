dcb678eb075060aa8dae0bd2920fb1948d8ada38
XeryusTC <armadillo@onenetbeyond.org>
2015-02-20 10:12:57 +0100

    Implement FPS movement

--- opengl/openglframework.c
+++ opengl/openglframework.c
@@ -29,14 +29,15 @@
 #include <GL/glut.h>
 #endif
 
-
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define TRUE 1
-#define FALSE 0
+typedef enum bool {
+	FALSE = 0,
+	TRUE = 1,
+} bool;
 
 typedef enum CONTROL_MODE {
 	MODE_ROTATE_CLICK,
@@ -45,8 +46,12 @@ typedef enum CONTROL_MODE {
 } CONTROL_MODE;
 
 /* CONSTANTS */
-const float rotscale=.5;
 const float PI=3.141592654;
+const float TORAD=3.141592654/180.0;
+const float rotscale=.5;
+const float fpsrotscale=.5;
+const float zoomdelta=.1;
+const float walkspeed=.1;
 
 GLfloat cubeVertices[8*3] = {-1,-1,-1, -1,-1, 1, -1, 1,-1,  1,-1,-1, -1, 1, 1,  1,-1, 1,  1, 1,-1,  1, 1, 1};
 GLubyte cubeIndices[2*12] = {
@@ -68,10 +73,13 @@ GLfloat cubeFaceColors[3*8] = {
 };
 
 int lastx=0, lasty=0;
-int leftmousedown=0, rightmousedown=0;
+bool leftmousedown=FALSE, rightmousedown=TRUE;
 
-float rotx=0, roty=0;
 CONTROL_MODE rotate_mode;
+float rotx=0, roty=0;
+float zoomlevel=5.0;
+float campos[3]; // x, y, z position
+float camrot[3]; // yaw, pitch, roll
 
 void display(void)
 {
@@ -79,9 +87,21 @@ void display(void)
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glColor3f(0.0f,0.0f,1.0f);
     glLoadIdentity();
-    gluLookAt(0.0, 0.0, 5.0,
-			  0.0, 0.0, 0.0,
-			  0.0, 1.0, 0.0);
+	if (rotate_mode == MODE_FPS) {
+		// Calculate direction to look in
+		float xlook, ylook, zlook;
+		xlook = sin(camrot[0] * TORAD);
+		ylook = sin(camrot[1] * TORAD);
+		zlook = sin((camrot[0] + 90) * TORAD);
+
+		gluLookAt(campos[0], campos[1], campos[2],
+		          campos[0]+xlook, campos[1]+ylook, campos[2]+zlook,
+				  0.0, 1.0, 0.0);
+	} else {
+	    gluLookAt(0.0, 0.0, zoomlevel,
+		          0.0, 0.0, 0.0,
+		          0.0, 1.0, 0.0);
+	}
 
 	// Set rotation
 	glMatrixMode(GL_MODELVIEW);
@@ -108,8 +128,34 @@ void display(void)
 void keyboard(unsigned char key, int x, int y)
 {
     switch (key) {
-        case 'q':
-        case 'Q':
+		case 'a': case 'A':
+			campos[0] += walkspeed * cos(camrot[0] * TORAD);
+			campos[2] -= walkspeed * sin(camrot[0] * TORAD);
+			break;
+		case 'd': case 'D':
+			campos[0] -= walkspeed * cos(camrot[0] * TORAD);
+			campos[2] += walkspeed * sin(camrot[0] * TORAD);
+			break;
+		case 's': case 'S':
+			campos[0] -= walkspeed * sin(camrot[0] * TORAD) * cos(camrot[1] * TORAD);
+			campos[1] -= walkspeed * sin(camrot[1] * TORAD);
+			campos[2] -= walkspeed * cos(camrot[0] * TORAD) * cos(camrot[1] * TORAD);
+			break;
+		case 'w': case 'W':
+			campos[0] += walkspeed * sin(camrot[0] * TORAD) * cos(camrot[1] * TORAD);
+			campos[1] += walkspeed * sin(camrot[1] * TORAD);
+			campos[2] += walkspeed * cos(camrot[0] * TORAD) * cos(camrot[1] * TORAD);
+			break;
+		case 'e': case 'E':
+			campos[1] -= walkspeed;
+			break;
+        case 'q': case 'Q':
+			// use Q to move up in FPS mode
+			if (rotate_mode == MODE_FPS) {
+				campos[1] += walkspeed;
+				break;
+			}
+			// use Q to exit when not in FPS mode
         case 27: // ESC key
             printf("Exiting...\n");
             exit(0);
@@ -128,8 +174,15 @@ void reshape(int w, int h)
 
 void mouseDelta(int x, int y, int *dx, int *dy)
 {
-    *dx = x - lastx;
-    *dy = y - lasty;
+	static bool firstrun=TRUE;
+	if (firstrun) {
+		*dx = 0;
+		*dy = 0;
+		firstrun = FALSE;
+	} else {
+	    *dx = x - lastx;
+    	*dy = y - lasty;
+	}
     lastx = x;
     lasty = y;
 }
@@ -140,6 +193,12 @@ void mouse(int button, int state, int x, int y)
 		leftmousedown = TRUE;
 	} else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
 		leftmousedown = FALSE;
+	} else if (button == 3 && rotate_mode != MODE_FPS) {
+		zoomlevel -= zoomdelta;
+	} else if (button == 4 && rotate_mode != MODE_FPS) {
+		zoomlevel += zoomdelta;
+	} else {
+		printf("Unhandled mouse event: %d %d %d %d\n", button, state, x, y);
 	}
 }
 
@@ -162,6 +221,9 @@ void passiveMotion(int x, int y)
 		// Moving mouse in x direction rotates around y axis
 		roty += dx * rotscale;
 		rotx += dy * rotscale;
+	} else if (rotate_mode == MODE_FPS) {
+		camrot[0] -= dx * fpsrotscale;
+		camrot[1] += dy * fpsrotscale;
 	}
 }
 
@@ -172,7 +234,7 @@ int main(int argc, char** argv)
 #endif
 
 	// Set default options
-	rotate_mode = MODE_ROTATE_PASSIVE;
+	rotate_mode = MODE_ROTATE_CLICK;
 	// Parse command line arguments
 	int i;
 	for (i=1; i<argc; ++i) {
@@ -183,6 +245,15 @@ int main(int argc, char** argv)
 		} else if (strcmp(argv[i], "-p")==0 || strcmp(argv[i], "--passive")==0) {
 			rotate_mode = MODE_ROTATE_PASSIVE;
 		}
+	}
+
+	if (rotate_mode == MODE_FPS) {
+		campos[0] = 0.0;
+		campos[1] = 0.0;
+		campos[2] = 5.0;
+		camrot[0] = 180.0;
+		camrot[1] = 0.0;
+		camrot[2] = 0.0;
 	}
 
 	// Create window
